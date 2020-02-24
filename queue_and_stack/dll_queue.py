a16600bed3c0ef1741bc49403f36ecbdd9f5c7b0
Greg <gsc229@gmail.com>
2020-02-24 16:38:10 -0500

    passing queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -21,4 +21,7 @@ class Queue:
             return None
 
     def len(self):
-        self.size
+        if self == None:
+            return self.size == 0
+        else:
+            return self.size
